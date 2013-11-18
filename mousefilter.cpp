44db23e785c98d0ebf1326e7999eeaa67d718a21
Uncrowned <link_92@list.ru>
2013-11-18 18:29:02 +0300

    mousefilter.cpp responsible for in game mouse click control, changing iterators, scenes, paragraphs and other

---
+++ mousefilter.cpp
@@ -0,0 +1,49 @@
+﻿#include "mousefilter.h"
+
+MouseFilter::MouseFilter(QObject *parent, GameWindow *window) :
+    QObject(parent)
+{
+    this->gameWindow = window;
+}
+
+/*virtual*/
+bool MouseFilter::eventFilter(QObject *, QEvent *event)
+{
+    if (event->type() == QEvent::MouseButtonPress) {
+        if (((QMouseEvent*)event)->button() == Qt::LeftButton) {
+            if (this->gameWindow->pTimer->isActive()) {
+                this->gameWindow->pTimer->stop();
+                this->gameWindow->finishParagraph();
+            } else {
+                this->gameWindow->pCount++;
+                if (!this->gameWindow->xmlDoc->isPExist(this->gameWindow->scene, this->gameWindow->pCount)) {
+                    this->gameWindow->sCount++;
+                    if (!this->gameWindow->xmlDoc->isSceneExist(
+                                this->gameWindow->xmlDoc->getChapter(this->gameWindow->chapter),
+                                this->gameWindow->sCount)
+                            ) {
+                        this->gameWindow->chapter++;
+                        if (!this->gameWindow->xmlDoc->isChapterExist(this->gameWindow->chapter)) {
+                            return false;
+                        }
+                        this->gameWindow->sCount = 0;
+                    }
+                    this->gameWindow->setScene();
+                    this->gameWindow->pCount = 0;
+                }
+                this->gameWindow->showParagraph(
+                            this->gameWindow->xmlDoc->getP(this->gameWindow->scene,
+                                                           this->gameWindow->pCount)
+                            );
+            }
+            return true;
+        }
+        if (((QMouseEvent*)event)->button() == Qt::RightButton) {
+            return true;
+        }
+        if (((QMouseEvent*)event)->button() == Qt::MiddleButton) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ mousefilter.h
@@ -0,0 +1,22 @@
+﻿#ifndef MOUSEFILTER_H
+#define MOUSEFILTER_H
+
+#include <QObject>
+#include "gamewindow.h"
+
+class GameWindow;
+
+class MouseFilter : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MouseFilter(QObject *parent = 0, GameWindow *window = 0);
+    
+protected:
+    virtual bool eventFilter(QObject *, QEvent *event);
+
+private:
+    GameWindow *gameWindow;
+};
+
+#endif // MOUSEFILTER_H
