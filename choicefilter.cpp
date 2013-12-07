aa2ade9c248dabd3067e9705e044316dda5f98a3
Uncrowned <link_92@list.ru>
2013-12-07 19:39:02 +0300

    now text color for choices changing while mouse on this

--- choicefilter.cpp
+++ choicefilter.cpp
@@ -10,18 +10,42 @@ ChoiceFilter::ChoiceFilter(QObject *parent, GameWindow *window) :
 bool ChoiceFilter::eventFilter(QObject *obj, QEvent *event)
 {
     QDomNode choiceNode;
+    QPalette palette;
+    QLabel *label;
     if (event->type() == QEvent::MouseButtonPress) {
         if (((QMouseEvent*)event)->button() == Qt::LeftButton) {
             choiceNode = this->gameWindow->xmlDoc->getChoiceList(this->gameWindow->scene)
                     .at(obj->objectName().toInt());
+
             if (!choiceNode.toElement().attribute("file").isEmpty()) {
                 this->gameWindow->setNewFile(choiceNode.toElement().attribute("file"));
             }
+
             this->gameWindow->sceneId = choiceNode.toElement().attribute("scene");
             this->gameWindow->choiceNotExist = true;
             this->gameWindow->setScene();
+
             return true;
         }
+    }
+
+    if (event->type() == QEvent::Enter) {
+        label = qobject_cast<QLabel*>(obj);
+        palette = label->palette();
+        palette.setColor(label->foregroundRole(), QColor(Qt::cyan));
+        label->setPalette(palette);
+
+        return true;
+    }
+
+    if (event->type() == QEvent::Leave) {
+        label = qobject_cast<QLabel*>(obj);
+        palette = label->palette();
+        palette.setColor(label->foregroundRole(), this->gameWindow->textColor);
+        qDebug() << this->gameWindow->textColor.value();
+        label->setPalette(palette);
+
+        return true;
     }
     return false;
 }
